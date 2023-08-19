d7571f19fab9f3e211a9e8cfdf2d55f57db99512
Magmusacy <xpfk.098@gmail.com>
2023-08-19 20:32:32 +0200

    Create gameBoard, displayController and Player

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const Player = (name, mark) => {
+    return {name, mark}
+}
+
+const gameBoard = (() => {
+    let board = [
+                // Populated for testing purposes
+                 ['', '', 'X'],
+                 ['O', 'X', ''],
+                 ['X', '', '']
+                ];
+    return {board}
+})();
+
+const displayController = (() => {
+    const drawBoard = () => {
+        const board = document.querySelector('#board');
+        board.innerHTML = "";
+        for (const row of gameBoard.board) {
+            for (const column of row) {
+                const newElement = `<div class="cell unselectable">${column}</div>`
+                board.insertAdjacentHTML("beforeend", newElement);
+            };
+        };
+    };
+
+    return {drawBoard};
+})();
+
+const gameFlow = (() => {
+    const startGame = () => {
+        displayController.drawBoard();
+    };
+
+    return {startGame};
+})();
+
+gameFlow.startGame();
