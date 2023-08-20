2d9067f4b8a539e23de1c0ea5718588b19801a4d
Magmusacy <xpfk.098@gmail.com>
2023-08-20 14:29:39 +0200

    Add logic for checking whether the board is in a tie or a player wins

--- js/main.js
+++ js/main.js
@@ -6,10 +6,53 @@ const gameBoard = (() => {
     let board = [
                 // Populated for testing purposes
                  ['', '', 'X'],
-                 ['O', 'X', ''],
+                 ['O', 'X', 'X'],
                  ['X', '', '']
                 ];
-    return {board}
+
+    const addMark = (row, col, mark) => {
+        if (
+            row >= 0 && row < 3 &&
+            col >= 0 && col < 3 &&
+            board[row][col] === ''
+           ) {
+            board[row][col] = mark;
+            return true
+        };
+        return false
+    };
+
+    const isBoardFull = () => {
+        return board.filter(row => row.filter(col => col === '').length).length === 0;
+    };
+
+    const checkForWinner = (playerMark) => {
+        // Check rows
+        for (const row of board) {
+            if (row[0] === playerMark && row[1] === playerMark && row[2] === playerMark) {
+                return true;
+            }
+        };
+
+        // Check columns
+        for (let row = 0; row < 3; row++) {
+            if (board[row][0] === playerMark && board[row][1] === playerMark && board[row][2] === playerMark) {
+                return true;
+            };
+        };
+
+        // Check diagonally
+        if (
+            board[0][0] === playerMark && board[1][1] === playerMark && board[2][2] === playerMark ||
+            board[0][2] === playerMark && board[1][1] === playerMark && board[2][0] === playerMark
+           ) {
+                return true;
+            };
+
+        return false;
+    };
+
+    return {board, addMark, isBoardFull, checkForWinner};
 })();
 
 const displayController = (() => {
